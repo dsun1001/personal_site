7da3aba54f658efbb7104379d5a91cdf15a8cda3
David Sun <davidthesun1001@gmail.com>
2022-10-22 18:25:43 -0400

    testing the changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,32 @@
-grunt.loadNpmTasks('grunt-aws-s3');
+module.exports = function(grunt) {
+    grunt.initConfig({
+      aws: grunt.file.readJSON("./aws-keys.json"),
+      aws_s3: {
+        options: {
+          accessKeyId: "<%= aws.AWSAccessKeyId %>",
+          secretAccessKey: "<%= aws.AWSSecretKey %>",
+          region: "<%= aws.region %>"
+        },
+        dist: {
+          options: {
+            bucket: "davidsun.link",
+            differential: true
+          },
+          files: [
+            {
+              expand: true,
+              cwd: "./",
+              src: ["**", 
+                "!aws-keys.json", 
+                "!Gruntfile.js"],
+              dest: ""
+            }
+          ]
+        }
+      }
+    });
+  
+    grunt.loadNpmTasks("grunt-aws-s3");
+
+    grunt.registerTask("deploy", "aws_s3");
+  };

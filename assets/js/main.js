3790af56ac3528fc82543808cdbff073d2ba2c25
David Sun <davidthesun1001@gmail.com>
2025-05-01 02:39:14 -0400

    Added top nav bar. Added in lightbox2 for image viewing support

--- assets/js/main.js
+++ assets/js/main.js
@@ -339,3 +339,12 @@
 						});
 
 })(jQuery);
+
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  }
